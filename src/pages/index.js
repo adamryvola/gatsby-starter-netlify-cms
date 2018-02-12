7568f07e8ade5be99ea2ad8818af1384a7034ad1
Adam Ryvola <ryvolaadam@gmail.com>
2018-02-12 21:28:46 +0100

    link style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,8 @@
 import React from "react";
-import Link from "gatsby-link";
-import Script from "react-load-script";
 import graphql from "graphql";
-import Content, { HTMLContent } from '../components/Content';
+import { HTMLContent } from '../components/Content';
 
 export default class IndexPage extends React.Component {
-  handleScriptLoad() {
-    if (typeof window !== `undefined` && window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      });
-    }
-    window.netlifyIdentity.init();
-  }
 
   render() {
     const { data } = this.props;
