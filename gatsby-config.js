1eca7dfdc65b58b4543572545e8e6be94cf33996
Adam Ryvola <ryvolaadam@gmail.com>
2018-02-18 15:26:51 +0100

    gatsby-netlify-cms plugin enabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,11 @@ module.exports = {
         plugins: [],
       },
     },
-      /*
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/index.js`,
       },
     },
-    */
   ],
 };
